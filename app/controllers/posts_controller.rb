43ae1e5046349c50365ec2d82983d767642eaad0
Ricky Yang <rickyyang987@gmail.com>
2019-03-16 10:30:54 -0400

    quick fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
-  before_action :find_post, only [:show, :update, :edit, :destroy]
+  # before_action :find_post, only [:show, :update, :edit, :destroy]
 
-  
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -11,9 +10,11 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post
     else
@@ -22,6 +23,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
 
@@ -39,6 +41,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
 
